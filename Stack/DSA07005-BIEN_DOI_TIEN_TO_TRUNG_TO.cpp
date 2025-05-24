fbe9f7184acccb1e3b4c9eef8f0c3102ce6a05e3
Phan Manh Cuong <cuongpm.b23cn115@stu.ptit.edu.vn>
2025-05-25 00:41:18 +0700

    Create DSA07005-BIEN_DOI_TIEN_TO_TRUNG_TO.cpp

---
+++ Stack/DSA07005-BIEN_DOI_TIEN_TO_TRUNG_TO.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		string s;
+		cin>>s;
+		stack<string> S;
+		for(int i=s.size()-1;i>=0;i--){
+			if(s[i]>='A'&&s[i]<='Z') S.push(string(1,s[i]));
+			else{
+				string a=S.top();S.pop();
+				string b=S.top();S.pop();
+				S.push('('+a+s[i]+b+')');
+			}
+		}
+		string c=S.top();
+		cout<<c<<endl;
+	}
+}
