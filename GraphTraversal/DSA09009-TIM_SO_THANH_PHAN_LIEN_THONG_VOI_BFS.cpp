b1d81953806cbbdd5424fd24cbd1af2584bd0d17
Phan Manh Cuong <cuongpm.b23cn115@stu.ptit.edu.vn>
2025-05-26 13:40:59 +0700

    Create DSA09009-TIM_SO_THANH_PHAN_LIEN_THONG_VOI_BFS.cpp

---
+++ GraphTraversal/DSA09009-TIM_SO_THANH_PHAN_LIEN_THONG_VOI_BFS.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+
+using namespace std;
+
+class Graph{
+private:
+	int n;
+	vector<vector<int>> adj;
+	vector<bool> visited;
+public:
+	Graph(int n):n(n){
+		adj.resize(n+1);
+		visited.resize(n+1,false);
+	}
+	void addEdge(int u,int v){
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	void bfs(int u){
+		queue<int> q;
+		q.push(u);
+		visited[u]=true;
+		while(!q.empty())
+		{
+			int temp=q.front();
+			q.pop();
+			for(int v: adj[temp])
+			{
+				if(!visited[v]){
+					visited[v]=true;
+					q.push(v);
+				}
+			}
+		}
+	}
+	int tinh_thanh_phan_lien_thong(){
+		int result=0;
+		for(int i=1;i<=n;i++)
+		{
+			if(!visited[i]){
+				result++;
+				bfs(i);
+			}
+		}
+		return result;
+	}
+};
+
+void solve()
+{
+	int n,k;
+	cin>>n>>k;
+	Graph g(n);
+	for(int i=0;i<k;i++)
+	{
+		int u,v;cin>>u>>v;
+		if(u>=1&&u<=n&&v>=1&&v<=n){
+			g.addEdge(u,v);
+		}
+	}
+	cout<<g.tinh_thanh_phan_lien_thong()<<endl;
+}
+
+int main()
+{
+	int t;cin>>t;while(t--) solve();
+	return 0;	
+}
