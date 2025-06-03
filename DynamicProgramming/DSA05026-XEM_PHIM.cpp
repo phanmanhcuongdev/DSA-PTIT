26189d585249a99969dcd8e6508742fa07f89eed
Phan Manh Cuong <cuongpm.b23cn115@stu.ptit.edu.vn>
2025-06-03 13:40:57 +0700

    Update DSA05026-XEM_PHIM.cpp

--- DynamicProgramming/DSA05026-XEM_PHIM.cpp
+++ DynamicProgramming/DSA05026-XEM_PHIM.cpp
@@ -4,7 +4,7 @@
 #include<algorithm>
 
 using namespace std;
-
+/*cách 2:
 void solve() {
     int c,n;
     cin>>c>>n;
@@ -23,6 +23,29 @@ void solve() {
             return;
         }
     }
+}*/
+
+void solve() {
+    int c,n;
+    cin>>c>>n;
+    vector<int> w(n);
+    for (int i=0;i<n;i++) cin>>w[i];
+    vector<int> dp(c+1,-1);
+    dp[0]=0;
+    for (int i=0;i<n;i++) {
+        for (int j=c;j>=w[i];j--) {
+            if (dp[j - w[i]] != -1) {
+                dp[j] = max(dp[j], dp[j - w[i]] + w[i]);
+            }
+        }
+    }
+    for (int j=c;j>=0;j--) {
+        if (dp[j]!=-1) {
+            cout<<j<<endl;
+            return;
+        }
+    }
+    cout<<0<<endl;
 }
 
 int main() {
