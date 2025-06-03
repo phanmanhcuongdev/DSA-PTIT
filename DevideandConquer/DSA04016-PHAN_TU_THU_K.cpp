52cae82a823a0b14ea291d4609bbf614ed023470
Phan Manh Cuong <cuongpm.b23cn115@stu.ptit.edu.vn>
2025-06-04 00:09:05 +0700

    Update DSA04016-PHAN_TU_THU_K.cpp

--- DevideandConquer/DSA04016-PHAN_TU_THU_K.cpp
+++ DevideandConquer/DSA04016-PHAN_TU_THU_K.cpp
@@ -27,3 +27,50 @@ int main() {
     }
     return 0;
 }
+/*
+Cách dùng Devide and conquer
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+int findKthElement(vector<int> &A,vector<int> &B,int k) {
+    int n=A.size(),m=B.size();
+    int i=0,j=0;
+    while (true) {
+        if (i==n) return B[j+k-1];
+        if (j==m) return A[i+k-1];
+        if (k==1) return min(A[i],B[j]);
+        int midA=min(k/2,n-i);
+        int midB=min(k/2,m-j);
+        if (A[i+midA-1]<B[j+midB-1]) {
+            i+=midA;
+            k-=midA;
+        }else {
+            j+=midB;
+            k-=midB;
+        }
+    }
+}
+
+void solve() {
+    int m,n,k;
+    cin>>m>>n>>k;
+    vector<int> A(m),B(n);
+    for (int i = 0; i < m; i++) cin >> A[i];
+    for (int i = 0; i < n; i++) cin >> B[i];
+
+    cout << findKthElement(A, B, k) << endl;
+}
+
+int main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
+*/
