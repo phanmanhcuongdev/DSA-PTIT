9dfc1ef669d5408096baa2dcfeed4a143b2ff35c
Phan Manh Cuong <cuongpm.b23cn115@stu.ptit.edu.vn>
2025-06-03 23:33:51 +0700

    Create DSA04016-PHAN_TU_THU_K.cpp

---
+++ DevideandConquer/DSA04016-PHAN_TU_THU_K.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        int m,n,k;
+        cin>>m>>n>>k;
+        multiset<int> M;
+        for (int i=0;i<m+n;i++) {
+            int x;
+            cin>>x;
+            M.insert(x);
+        }
+        int index=0;
+        for (int m:M) {
+            index++;
+            if (index==k) {
+                cout<<m<<endl;
+                break;
+            }
+        }
+    }
+    return 0;
+}
