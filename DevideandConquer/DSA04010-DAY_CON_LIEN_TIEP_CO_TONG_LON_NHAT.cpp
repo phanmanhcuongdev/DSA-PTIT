482a09711a655678721e1417d9798a19b3a9a3e1
Phan Manh Cuong <cuongpm.b23cn115@stu.ptit.edu.vn>
2025-06-03 21:39:11 +0700

    Update DSA04010-DAY_CON_LIEN_TIEP_CO_TONG_LON_NHAT.cpp

--- DevideandConquer/DSA04010-DAY_CON_LIEN_TIEP_CO_TONG_LON_NHAT.cpp
+++ DevideandConquer/DSA04010-DAY_CON_LIEN_TIEP_CO_TONG_LON_NHAT.cpp
@@ -1,51 +1,48 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
+
 using namespace std;
 
-int maxCrossingSum(int A[], int l, int m, int r) {
-    int left_sum = -1e9, right_sum = -1e9;
-    int sum = 0;
-    
-    for (int i = m; i >= l; i--) {
-        sum += A[i];
-        left_sum = max(left_sum, sum);
+int maxCrossingArray(vector<int> &Arr,int l,int m,int r) {
+    int right=-1e9;int left=-1e9 ;
+    int sum=0;
+    for (int i=m;i>=l;--i) {
+        sum+=Arr[i];
+        left=max(left,sum);
     }
-    
-    sum = 0;
-    for (int i = m + 1; i <= r; i++) {
-        sum += A[i];
-        right_sum = max(right_sum, sum);
+    sum=0;
+    for (int i=m+1;i<=r;i++) {
+        sum+=Arr[i];
+        right=max(right,sum);
     }
-
-    return left_sum + right_sum; 
+    return left+right;
 }
 
-int maxSubArraySum(int A[], int l, int r) {
-    if (l == r) {
-        return A[l];
+int maxSumArray(vector<int> &Arr,int l,int r) {
+    if (l==r) {
+        return Arr[l];
     }
+    int m=(l+r)/2;
+    int left_sum=maxSumArray(Arr,l,m);
+    int right_sum=maxSumArray(Arr,m+1,r);
+    int cross_sum=maxCrossingArray(Arr,l,m,r);
+    return max(left_sum,max(cross_sum,right_sum));
+}
 
-    int m = (l + r) / 2;
-
-    int left_sum = maxSubArraySum(A, l, m);
-    int right_sum = maxSubArraySum(A, m + 1, r);
-    int crossing_sum = maxCrossingSum(A, l, m, r);
-
-    return max(left_sum, max(right_sum, crossing_sum));
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> Arr(n);
+    for (int i=0;i<n;i++) {
+        cin>>Arr[i];
+    }
+    cout<<maxSumArray(Arr,0,n-1)<<endl;
 }
 
 int main() {
-    int t; 
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n; 
-        int A[100]; 
-        for (int i = 0; i < n; i++) {
-            cin >> A[i]; 
-        }
-        cout << maxSubArraySum(A, 0, n - 1) << endl; 
-    }
-
+    int t;
+    cin>>t;
+    while (t--) solve();
     return 0;
 }
