239b4bccd6e70dd5a0a1545f3672e4ec87fe4515
Phan Manh Cuong <cuongpm.b23cn115@stu.ptit.edu.vn>
2025-06-04 10:43:32 +0700

    Update DSA04018-DEM_SO_0.cpp

--- DevideandConquer/DSA04018-DEM_SO_0.cpp
+++ DevideandConquer/DSA04018-DEM_SO_0.cpp
@@ -1,21 +1,26 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+
 using namespace std;
-int main(){
-    int t;cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        int A[n];
-        bool k=true;
-        for(int i=0;i<n;i++) cin>>A[i];
-        for(int i=0;i<n;i++){
-            if(A[i]==1){
-                cout<<i<<endl;
-                k=false;
-                break;
-            }
-        }
-        if(k==true) cout<<0<<endl;
-    }
-    
+
+int find(const vector<int>& A,int l,int r) {
+    if (l>=r) return l;
+    int m=(l+r)/2;
+    if (A[m]==1) return find(A,l,m);
+    else return find(A,m+1,r);
+    return -1;
+}
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> ex(n);
+    for (int i=0;i<n;i++) cin>>ex[i];
+    ex.push_back(1);
+    cout<<find(ex,0,n)<<endl;
+}
+
+int main() {
+    int t;cin>>t;while(t--) solve();
+    return 0;
 }
