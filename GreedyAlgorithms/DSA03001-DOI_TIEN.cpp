04b120b3604e1ca7eb616db9d210144c12b10602
Phan Manh Cuong <cuongpm.b23cn115@stu.ptit.edu.vn>
2025-06-04 22:17:34 +0700

    Update DSA03001-DOI_TIEN.cpp

--- GreedyAlgorithms/DSA03001-DOI_TIEN.cpp
+++ GreedyAlgorithms/DSA03001-DOI_TIEN.cpp
@@ -3,7 +3,7 @@
 #include<algorithm>
 
 using namespace std;
-
+/*
 #define nhap(n) int n;cin>>n;
 #define t() int t;cin>>t;while(t--)
 #define FOR(i,x,y) for(int i=x;i<y;i++)
@@ -26,5 +26,28 @@ int main(){
     {
         nhap(n);
         money_value(n);
+    }
+}*/
+
+vector<int> money_value={1, 2, 5, 10, 20, 50, 100, 200, 500, 1000};
+
+void solve() {
+    int n;cin>>n;
+    int result=0;
+    for (int i=9;i>=0;i--) {
+        if (n==0) break;
+        if (n>=money_value[i]) {
+            result+=n/money_value[i];
+            n%=money_value[i];
+        }
+    }
+    cout<<result<<endl;
+}
+
+int main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        solve();
     }
 }
