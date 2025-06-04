84a35280eca3d2049ad884d2902c56d24e7e888d
Phan Manh Cuong <cuongpm.b23cn115@stu.ptit.edu.vn>
2025-06-04 17:50:19 +0700

    Update DSA04022-KY_TU_THU_K_TRONG_XAU.cpp

--- DevideandConquer/DSA04022-KY_TU_THU_K_TRONG_XAU.cpp
+++ DevideandConquer/DSA04022-KY_TU_THU_K_TRONG_XAU.cpp
@@ -3,11 +3,12 @@
 #include<algorithm>
 
 using namespace std;
-
+//Cách 1
 int load(int k,int n) {
     if (k%2==1) return n;
     else return load(k/2,n+1);
 }
+//Cách 2
 char load(int n,int k) {
     while (true) {
         int m=(1<<(n-1));
@@ -26,7 +27,7 @@ int main() {
     while (t--) {
         int n,k;
         cin>>n>>k;
-        cout<<char('A'+load(k,0))<<endl;
-        cout<<load(n,k)<<endl;
+        cout<<char('A'+load(k,0))<<endl;//cách 1
+        cout<<load(n,k)<<endl;//cách 2
     }
 }
